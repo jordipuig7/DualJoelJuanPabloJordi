272f34ef65a9eee97c08c86481a8e13400aa9135
Joel <jcosta8@boscdelacoma.cat>
2019-11-07 15:59:30 +0100

    Commit de prova

--- Projecte-DUAL/Projecte-DUAL/Connector.py
+++ Projecte-DUAL/Projecte-DUAL/Connector.py
@@ -3,3 +3,6 @@ import mysql.connector;
 dbConnection = mysql.connector.connect(user='root', password='dualjjj',
                               host='127.0.0.1',
                               database='dualprojecte')
+
+
+JuanPa funciona cabron
